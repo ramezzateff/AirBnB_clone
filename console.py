0370780d0879f50fb0246966381408d81906af56
root <root@DESKTOP-7D25GVU.localdomain>
2024-05-19 15:42:36 +0300

    oo

--- console.py
+++ console.py
@@ -1,11 +1,34 @@
 #!/usr/bin/python3
-'''contains the entry point of the command interpreter'''
- 
+'Module for the entry  interpreter'
 import cmd
-
+from models.base_model import BaseModel
 class HBNBCommand(cmd.Cmd):
-	'''
-	'''
+    'its my interpreter'
+    prompt = '(hbnb)'
+    def do_quiet(self, arg):
+        'Quit command to exit the program'
+        return True
+    def do_EOF(self, arg):
+        'for handle end line'
+        print()
+        return True
+    def emptylines(self):
+        'empty lin + != anything'
+        return ''
+    def do_creat(self, arg):
+        'creat new instance, save Json and print id'
+        if arg == '':
+            print("** class name missing **")
+        if arg not in BaseModel:
+            print("create MyModel")
+        else:
+            new_instannce = BaseModel()
+            new_instannce.save()
+            print(new_instannce.id)
+
+
+
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
