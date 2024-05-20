82a517cfe6f28d1b02ece3e69d4e371db20bf758
root <root@DESKTOP-7D25GVU.localdomain>
2024-05-20 07:38:16 +0300

    fkdj

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Initializes the package"""
 from models.engine.file_storage import FileStorage
+import models
 storage = FileStorage()
 storage.reload()
--- models/user.py
+++ models/user.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-'''This model creates a User class'''
-from models.base_model import base_model
-
-class user(BaseModel):
-    '''Clas for managing user objects'''
-
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+"""module creates User class"""
+from models.base_model import BaseModel
+class User(BaseModel):
+	"""
+    Inherting from BaseModel
+    Setting class attribute
+    """
+	email = ""
+	password = ""
+	first_name = ""
+	last_name = ""
