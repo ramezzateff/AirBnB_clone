94dc2d6258c461de12d31e8c0c6911db61ce4ea2
root <root@DESKTOP-7D25GVU.localdomain>
2024-05-20 07:54:07 +0300

    dljf

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 ''' create unique FileStorage'''
 import models
-from models.engine.file_storage import FileStorage
-storage = FileStorage()
+import models.engine
+import models.engine.file_storage
+storage = models.engine.file_storage.FileStorage()
 storage.reload()
