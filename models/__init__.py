00652862b2810530785790e1cbf5f3ace7203482
root <root@DESKTOP-7D25GVU.localdomain>
2024-05-20 07:40:32 +0300

    tasl

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 """Initializes the package"""
 from models.engine.file_storage import FileStorage
-import models
+from models.engine import file_storage
+from models.base_model import BaseModel
+from models.user import User
+from models.review import Review
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.state import State
 storage = FileStorage()
 storage.reload()
