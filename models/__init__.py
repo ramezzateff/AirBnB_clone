18b7b761e0f103cd77524f1ed5185d556da6c5bf
root <root@DESKTOP-7D25GVU.localdomain>
2024-05-20 08:06:53 +0300

    jdslf

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 ''' create unique FileStorage'''
 import models
+from models.base_model import BaseModel
 import models.engine
 import models.engine.file_storage
 storage = models.engine.file_storage.FileStorage()
