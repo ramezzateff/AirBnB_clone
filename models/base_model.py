9c08738ff30f39db011679dbd417b1a1d69fc7d5
root <root@DESKTOP-7D25GVU.localdomain>
2024-05-21 10:40:45 +0300

    edit console by Ali Elshaer

--- models/base_model.py
+++ models/base_model.py
@@ -18,8 +18,7 @@ class BaseModel:
         if kwargs is not None and kwargs != {}:
             for key in kwargs:
                 if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(
-                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                    self.__dict__["created_at"] = datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 if key == "updated_at":
                     self.__dict__["updated_at"] = datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 else:
