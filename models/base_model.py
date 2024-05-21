7866022e5b37bff16abb9d252d8626ec91976629
root <root@DESKTOP-7D25GVU.localdomain>
2024-05-21 12:18:24 +0300

    its cup of coffe

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """BaseModel defines attributes and methods for all"""
-import models
-import json
 import uuid
 from datetime import datetime
 from models import storage
@@ -18,7 +16,8 @@ class BaseModel:
         if kwargs is not None and kwargs != {}:
             for key in kwargs:
                 if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(kwargs["created_at"].isoformat(), "%Y-%m-%dT%H:%M:%S.%f")
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 if key == "updated_at":
                     self.__dict__["updated_at"] = datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 else:
@@ -27,13 +26,14 @@ class BaseModel:
             self.id = str(uuid.uuid4())
 
         self.created_at = datetime.now()
-        print(f"time of created_at \n{self.created_at}")
-        self.update_at = datetime.now()
-        print(f"time update_at \n{self.update_at}")
+        #print(f"time of created_at \n{self.created_at}")
+        self.updated_at = datetime.now()
+        #print(f"time updated_at \n{self.updated_at}")
+        storage.new(self)
 
     def save(self):
         """update instance attribute updateds"""
-        self.update_at = datetime.now()
+        self.updated_at = datetime.now()
         storage.save()
 
     
@@ -45,8 +45,8 @@ class BaseModel:
         """
         cop = self.__dict__.copy()
         cop["__class__"] = type(self).__name__
-        cop["create_at"] = cop["created_at"].isoformat()
-        cop["update_at"] = self.update_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        cop["created_at"] = cop["created_at"].isoformat()
+        cop["updated_at"] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
         return cop
 
     def __str__(self):
