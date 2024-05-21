a8ac7cd3283501184a47c8c825ebe9bcbc178376
root <root@DESKTOP-7D25GVU.localdomain>
2024-05-22 01:47:46 +0300

    edit BaseModel By Ali Elshaer

--- models/base_model.py
+++ models/base_model.py
@@ -3,40 +3,29 @@
 import uuid
 from datetime import datetime
 from models import storage
+
 class BaseModel:
     """Base class for inheritance by all other classes"""
+    
     def __init__(self, *args, **kwargs):
-        """Sets up instance attributes during initialization
-
-        args:
-            - *args: list of arguments
-            - **kwargs __dict__ of key:values arguments
-        """
-
-        if kwargs is not None and kwargs != {}:
-            for key in kwargs:
-                if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(
-                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                if key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                else:
-                    self.__dict__[key] = kwargs[key]
+        """Sets up instance attributes during initialization"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-
-        self.created_at = datetime.now()
-        #print(f"time of created_at \n{self.created_at}")
-        self.updated_at = datetime.now()
-        #print(f"time updated_at \n{self.updated_at}")
-        storage.new(self)
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
     def save(self):
         """update instance attribute updateds"""
         self.updated_at = datetime.now()
         storage.save()
 
-    
     def to_dict(self):
         """
         returns copy of dictionary
@@ -45,8 +34,8 @@ class BaseModel:
         """
         cop = self.__dict__.copy()
         cop["__class__"] = type(self).__name__
-        cop["created_at"] = cop["created_at"].isoformat()
-        cop["updated_at"] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        cop["created_at"] = self.created_at.isoformat()
+        cop["updated_at"] = self.updated_at.isoformat()
         return cop
 
     def __str__(self):
